f62c2777246614488231f99f63b861a04708b202
Anil Bas <anil.bas@marmara.edu.tr>
2017-12-04 15:39:20 +0000

    Bugfix on the backward pass

--- layer/vl_nngrid.m
+++ layer/vl_nngrid.m
@@ -3,16 +3,13 @@ function [ y ] = vl_nngrid( X,dzdy )
 % X	: 1 x 2 x N x b
 % y : 2 x Ho x Wo x b
 nbatch = size(X,4);
-grid_dim = sqrt(size(X, 3));
+grid_dim = sqrt(size(X, 3)); % Usually this will be 224
 if nargin<2
     %forward
     
-%     Xo = zeros(size(X),'single');
-%     for i=1:nbatch
-%     Xo(:,:,:,i)= (X(:,:,:,i) - 112)/112;
-%     end
-
-    Xo = (X - 112)/112; % This has to stay 112: The input image is 224x224, and we're centering the grid here.
+    % We're centering the grid here and rescaling to a [-1 1] coordinate
+    % system. The input image is 224x224x3.
+    Xo = (X - 112)/112; 
     
     Xo = reshape(Xo,2,grid_dim,grid_dim,nbatch);
     newgrid(1,:,:,:) = -Xo(2,:,:,:);
@@ -25,7 +22,7 @@ else
     Yo(1,:,:,:) = dY(2,:,:,:);
     Yo(2,:,:,:) =  -dY(1,:,:,:);
     Yo = reshape(Yo,1,2,grid_dim^2,nbatch);
-    y = (1/grid_dim).*Yo;
+    y = (1/112).*Yo;
 end
 
 end
